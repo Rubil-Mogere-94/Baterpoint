1a500f221e15c93c634cb0dd4a44ef1e3d2da065
Rubil Mogere <mogererubil@gmail.com>
2026-02-01 14:42:30 +0300

    feat: re-implement CreateListingForm using MUI components

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,9 @@
-import { Container, Typography } from "@mui/material";
+import CreateListingFormMui from "@/components/forms/CreateListingFormMui";
 
 export default function Home() {
   return (
-    <Container>
-      <Typography variant="h1" component="h1" gutterBottom>
-        Welcome to BarterPoint
-      </Typography>
-      <Typography variant="body1">
-        Trade goods and services across Kenya
-      </Typography>
-    </Container>
+    <div>
+      <CreateListingFormMui />
+    </div>
   );
 }
