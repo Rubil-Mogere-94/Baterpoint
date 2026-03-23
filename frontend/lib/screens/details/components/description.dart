373f129c7b886e6c5f93e6547cbe9a3966728ada
Rubil Mogere <mogererubil@gmail.com>
2026-03-23 10:31:02 +0300

    new commit

--- frontend/lib/screens/details/components/description.dart
+++ frontend/lib/screens/details/components/description.dart
@@ -11,7 +11,7 @@ class Description extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(vertical: kDefaultPaddin),
-        Column(
+      child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
             Text(
